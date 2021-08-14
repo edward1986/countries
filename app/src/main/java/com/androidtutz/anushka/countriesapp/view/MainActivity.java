9b6bbeb9cba22ea58826fbe1f721a06ae4c15661
Lorilla Family <edwardlorilla1986@gmail.com>
2021-08-15 00:38:58 +0800

    first commit

---
+++ app/src/main/java/com/androidtutz/anushka/countriesapp/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.androidtutz.anushka.countriesapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidtutz.anushka.countriesapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
